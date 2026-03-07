21ad0bd1c8ac518bc6b893907f890c6d4a29af1a
lobotomyx <lbatvt@gmail.com>
2026-03-07 11:47:17 -0500

    Add glm vector and matrix types to Quaternion.hpp

--- lua-api/lib/include/datatypes/Quaternion.hpp
+++ lua-api/lib/include/datatypes/Quaternion.hpp
@@ -7,10 +7,20 @@
 
 #include "ScriptPrerequisites.hpp"
 #include <uevr/API.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <glm/vec3.hpp>
 
 namespace lua::datatypes {
     using Quaternionf = glm::quat;
     using Quaterniond = glm::dquat;
+    using Matrix4x4f = glm::mat4;
+    using Matrix4x4d = glm::dmat4;
+    using Vector3f = glm::vec3;
+    using Vector3d = glm::dvec3;
+    using Vector4f = glm::vec4;
+    using Vector4d = glm::dvec4;
 
     void bind_quaternions(sol::state_view& lua);
 }
